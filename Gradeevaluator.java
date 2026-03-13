1bf5251a6e9648e226a7eafb7491d5c6571a72ad
purnima <kettelpurnima@gmail.com>
2026-03-13 16:23:47 +0545

    aaa

---
+++ Gradeevaluator.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Gradeevaluator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Gradeevaluator
+{
+   public static void main(String[] args){
+   int grade =48;
+   String result = grade>=40? "pass" : "fail";
+   System.out.println("Grade:\t"+ grade);
+   System.out.println("Result:\t"+result);
+   }
+    
+}
